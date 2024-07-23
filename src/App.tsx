ee24a878cd9708eae58effae35a040ba3413fc77
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-07-22 19:51:20 -0500

    Change style in dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const App: React.FC = () => {
     <div className="App">
       <div className="container">
         {isAuthenticated ? (
-          <Dashboard />
+          <Dashboard currentUser={currentUser} />
         ) : isRegistering ? (
           <RegisterForm onRegister={handleRegister} />
         ) : (
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,32 +2,39 @@
 import React from 'react';
 import './Dashboard.css';
 
-const Dashboard: React.FC = () => {
+interface DashboardProps {
+  currentUser: string | null;
+}
+
+const Dashboard: React.FC<DashboardProps> = ({ currentUser }) => {
   return (
-    <div className="dashboard">
-      <div className="option">
-        <i className="icon user"></i>
-        <span>Perfil</span>
-      </div>
-      <div className="option">
-        <i className="icon database"></i>
-        <span>Ver datos</span>
-      </div>
-      <div className="option">
-        <i className="icon bracelet"></i>
-        <span>Pulsera</span>
-      </div>
-      <div className="option">
-        <i className="icon password"></i>
-        <span>Contraseñas</span>
-      </div>
-      <div className="option">
-        <i className="icon tutorial"></i>
-        <span>Tutorial</span>
-      </div>
-      <div className="option">
-        <i className="icon settings"></i>
-        <span>Opciones</span>
+    <div className="dashboard-container">
+      <h1 >Welcome, {currentUser}</h1>
+      <div className="dashboard">
+        <div className="option">
+          <i className="icon user"></i>
+          <span>Perfil</span>
+        </div>
+        <div className="option">
+          <i className="icon database"></i>
+          <span>Ver datos</span>
+        </div>
+        <div className="option">
+          <i className="icon bracelet"></i>
+          <span>Pulsera</span>
+        </div>
+        <div className="option">
+          <i className="icon password"></i>
+          <span>Contraseñas</span>
+        </div>
+        <div className="option">
+          <i className="icon tutorial"></i>
+          <span>Tutorial</span>
+        </div>
+        <div className="option">
+          <i className="icon settings"></i>
+          <span>Opciones</span>
+        </div>
       </div>
     </div>
   );
